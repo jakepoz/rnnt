2eb175e624a855561ec7011015250f0fd9d26983
Jake <jakepoz@gmail.com>
2024-03-12 20:59:11 +0000

    Cleaning up, adding featurizer and joint network

---
+++ rnnt/featurizer.py
@@ -0,0 +1,32 @@
+import torch
+import torchaudio
+import math
+
+_decibel = 2 * 20 * math.log10(torch.iinfo(torch.int16).max)
+_gain = pow(10, 0.05 * _decibel)
+
+
+def _piecewise_linear_log(x):
+    x = x * _gain
+    x[x > math.e] = torch.log(x[x > math.e])
+    x[x <= math.e] = x[x <= math.e] / math.e
+    return x
+
+
+class NormalizedMelSpectrogram(torchaudio.transforms.MelSpectrogram):
+    def __init__(self, apply_linear_log: bool=True, mean: float=15.0, invstddev: float=0.25, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_linear_log = apply_linear_log
+        self.mean = mean
+        self.invstddev = invstddev
+
+
+    def forward(self, waveform):
+        mel_spec = super().forward(waveform)
+
+        if self.apply_linear_log:
+            mel_spec = _piecewise_linear_log(mel_spec + 1e-6)
+
+        mel_spec = (mel_spec - self.mean) * self.invstddev
+
+        return mel_spec
---
+++ rnnt/joint.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn.functional as F
+
+class JointNetwork(torch.nn.Module):
+    def __init__(self, audio_features: int, text_features: int, hidden_features: int, num_classes: int):
+        super(JointNetwork, self).__init__()
+        self.audio_ln = torch.nn.Linear(audio_features, hidden_features)
+        self.text_ln = torch.nn.Linear(text_features, hidden_features)
+
+        self.joint_dropout = torch.nn.Dropout(0.2)
+        self.joint_ln = torch.nn.Linear(hidden_features, num_classes + 1)
+
+        self.blank_idx = num_classes
+
+    # Use this version to do training on a batch, where you need to calculate every combination of joint and decoder states
+    # Audio Frame expected to be [N, Length, Featuers]
+    # Text Frame expected to be [N, Length, Features]
+    def forward(self, audio_frame, text_frame):        
+        audio_frame = self.audio_ln(audio_frame)
+        text_frame = self.text_ln(text_frame)
+
+        audio_frames = audio_frame.unsqueeze(2)
+        text_frames = text_frame.unsqueeze(1)
+
+        joint_frames = audio_frames + text_frames
+
+        joint_frames = F.gelu(joint_frames)
+        joint_frames = self.joint_dropout(joint_frames)
+
+        return self.joint_ln(joint_frames)
+    
+    # Use this version to export the model and just do inference on a single thing at a time
+    # Audio Frame expected to be [N, Length, Featuers]
+    # Text Frame expected to be [N, Length, Features]
+    def single_forward(self, audio_frame, text_frame):
+        audio_frame = self.audio_ln(audio_frame)
+        text_frame = self.text_ln(text_frame)
+
+        joint_frame = audio_frame + text_frame
+
+        joint_frame = F.gelu(joint_frame)
+        joint_frame = self.joint_dropout(joint_frame)
+
+        return self.joint_ln(joint_frame)
+        
