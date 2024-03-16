10623be7ce10b6dfc9c68369afe6efcd3be49533
Jake <jakepoz@gmail.com>
2024-03-16 17:03:56 +0000

    Optuna fixups

--- rnnt/optuna.py
+++ rnnt/optuna.py
@@ -6,15 +6,13 @@ from rnnt.train import train
 
 def objective(trial):
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
-    beta0 = trial.suggest_float("beta0", 0.9, 0.9999, log=True)
+    beta0 = trial.suggest_float("beta0", 0.8, 0.9999, log=True)
     beta1 = trial.suggest_float("beta1", beta0, 0.9999, log=True)
-    apply_linear_log = trial.suggest_categorical("apply_linear_log", [True, False])
-    featurize_mean = trial.suggest_float("featurize_mean", 0.0, 30.0)
-    featurize_invstddev = trial.suggest_float("featurize_invstddev", 0.1, 0.5)
+
     lstm_dropout = trial.suggest_float("lstm_dropout", 0.0, 0.5)
     weight_decay = trial.suggest_float("weight_decay", 1e-3, 10, log=True)
     optimizer_eps = trial.suggest_float("optimzier_eps", 1e-9, 1e-5, log=True)
-    
+
     with initialize(config_path="config"):
         # Load your existing configuration
         cfg = compose(config_name="basic_sp.yaml")
@@ -25,10 +23,6 @@ def objective(trial):
         cfg.training.optimizer.weight_decay = weight_decay
         cfg.training.optimizer.eps = optimizer_eps
 
-        cfg.featurizer.apply_linear_log = apply_linear_log
-        cfg.featurizer.mean = featurize_mean
-        cfg.featurizer.invstddev = featurize_invstddev
-        
         cfg.predictor.lstm_dropout = lstm_dropout
         
         # Run training with the current set of hyperparameters
@@ -45,6 +39,8 @@ def objective(trial):
 
 
 if __name__ == "__main__":
+
+
     study = optuna.create_study(study_name="single_epoch_base", 
                                 direction="minimize",
                                 storage= optuna.storages.RDBStorage(url="postgresql://optuna_user:password@localhost/optuna_db"),
