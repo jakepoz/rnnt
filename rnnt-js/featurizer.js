9262f41b8f0314c44110f2a92e8ce34cdcee76e6
Jake <jakepoz@gmail.com>
2024-04-03 16:51:42 +0000

    Writing JS featurizer streamer with unit test

--- rnnt-js/featurizer.js
+++ rnnt-js/featurizer.js
@@ -28,10 +28,42 @@ export function featurizer(waveform) {
 
     let spec_f = tf.pow(tf.abs(stft_f), 2.0);
 
-    spec_f = _piecewise_linear_log(tf.add(spec_f, 1e-6));
+    // TODO Still figuring out the piecewise linear log part, it's too sensitive to numerical imprecision between tfjs and torch
+    //spec_f = _piecewise_linear_log(tf.add(spec_f, 1e-6));
+
+    spec_f = tf.log(tf.add(spec_f, 1e-6));
 
     // Hardcoded mean/stddev
     spec_f = tf.mul(tf.add(spec_f, -15.0), 0.25);
 
     return spec_f;
+}
+
+//Applies the featurizer as new samples come in and returns the newly generated features
+export class FeatureStreamer {
+    constructor() {
+        this.buffer = new Float32Array();
+
+        this.n_fft = 400;
+        this.hop_length = 160;
+        this.win_length = 400;
+
+        this.chunk_size = 1280;
+        this.overlap = this.win_length - this.hop_length;
+    }
+
+    process(waveform) {
+        if (waveform && waveform.length > 0) {
+            this.buffer = new Float32Array([...this.buffer, ...waveform]);
+        }
+
+        if (this.buffer.length < this.chunk_size + this.overlap) {
+            return null;
+        }
+
+        let waveform_tensor = tf.tensor1d(this.buffer.slice(0, this.chunk_size + this.overlap));
+        this.buffer = this.buffer.slice(this.chunk_size);
+
+        return featurizer(waveform_tensor);
+    }
 }
--- rnnt-js/featurizer.test.js
+++ rnnt-js/featurizer.test.js
@@ -1,29 +1,72 @@
 //import * as tf from '@tensorflow/tfjs-node';
 import * as tf from '@tensorflow/tfjs';
 
-import { featurizer } from './featurizer';
+import { featurizer, FeatureStreamer } from './featurizer';
 
 describe('featurizer function', () => {
-    it('should correctly compute the STFT of the input waveform', () => {
+    // it('should correctly compute the STFT of the input waveform', () => {
+    //     // Create a mock waveform as a 1D Tensor. This is a simplistic example.
+    //     const mockWaveform = tf.ones([1600], 'float32');
+
+    //     // Call the featurizer function
+    //     const output = featurizer(mockWaveform);
+
+    //     console.log(output.shape);
+    //     output.print();
+    // });
+
+    // it('should correctly compute for a reference sine wave', () => {
+    //     // Make a 4khz sine wave for 16000 samples
+    //     // audio = torch.sin(torch.linspace(0, 2 * 3.14159 * 4000, 16000))
+    //     const audio = tf.linspace(0, 2 * 3.14159 * 4000, 16000);
+    //     const audioSin = tf.sin(audio);
+
+    //     // Call the featurizer function
+    //     const output = featurizer(audioSin);
+
+    //     console.log(output.shape);
+    //     output.print();
+    // });
+
+    it('should match performance in streaming and non-streaming mode', () => {
         // Create a mock waveform as a 1D Tensor. This is a simplistic example.
-        const mockWaveform = tf.ones([10544], 'float32');
+        const mockWaveformSize = 32000;
+        const mockWaveform = new Float32Array(mockWaveformSize);
+        for (let i = 0; i < mockWaveformSize; i++) {
+            mockWaveform[i] = Math.random();
+        }
+ 
+        // Call the featurizer function on the whole output
+        const output = featurizer(tf.tensor1d(mockWaveform));
 
-        // Call the featurizer function
-        const output = featurizer(mockWaveform);
+        console.log("non-streaming output shape: ", output.shape);
 
-        console.log(output.shape);
-        output.print();
+        // Call the streamer once, it should match on the first N frames
+        let streamer = new FeatureStreamer();
+        let outputStreamer = streamer.process(mockWaveform);
 
-        // // Basic checks to ensure the output is of expected shape and type
-        // expect(output instanceof tf.Tensor).toBeTruthy();
-        // expect(output.shape.length).toBeGreaterThanOrEqual(2); // STFT output should be at least a 2D tensor
+        console.log("streaming output shape: ", outputStreamer.shape);  
 
-        // // Optionally, check the specifics of the shape, which depends on your featurizer's settings
-        // // This is just an example and might need to be adjusted based on actual output
-        // const expectedRows = Math.floor((1000 - 400) / 160) + 1;
-        // const expectedCols = 512 / 2 + 1;
-        // expect(output.shape).toEqual([expectedRows, expectedCols]);
+        let diff = tf.sub(output.slice(0, outputStreamer.shape[0]), outputStreamer).abs().sum().arraySync();
+        console.log("Difference between streaming and non-streaming: ", diff);
+        expect(diff).toBeLessThan(1e-6);
 
-   
+        // Now make a new streamer, and feed in the mock waveform in chunks and accumulate all the features
+        streamer = new FeatureStreamer();
+        let outputAccum = tf.zeros([0, 201]);
+        const streamerInputSize = 320;
+
+        for (let i = 0; i < mockWaveformSize; i += streamerInputSize) {
+            outputStreamer = streamer.process(mockWaveform.slice(i, i + streamerInputSize));
+            if (outputStreamer != null) {
+                outputAccum = tf.concat([outputAccum, outputStreamer], 0);
+            }
+        }
+
+        console.log("streaming output shape: ", outputAccum.shape);
+
+        diff = tf.sub(output.slice(0, outputAccum.shape[0]), outputAccum).abs().sum().arraySync();
+        console.log("Difference between streaming and non-streaming: ", diff);
+        expect(diff).toBeLessThan(1e-6);
     });
 });
